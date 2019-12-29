976684e43530bcf56aa3e7f98227aebec1f7ed16
XiaoHang <qs_yibunong@163.com>
2019-12-29 13:27:12 +0800

    add index notes

--- website/src/main/java/com/xh/learn/website/webProducts/IndexController.java
+++ website/src/main/java/com/xh/learn/website/webProducts/IndexController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * 首页
+ */
 @Controller
 public class IndexController {
 
